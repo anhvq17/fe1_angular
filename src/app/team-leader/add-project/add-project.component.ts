9738bfcce6ebdcd97f2158f1b9d8b4556bc0422c
Vu Quoc Anh <hinm.aaaa@gmail.com>
2025-03-29 10:10:24 +0700

    update validate

--- src/app/team-leader/add-project/add-project.component.ts
+++ src/app/team-leader/add-project/add-project.component.ts
@@ -1,43 +1,64 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-project',
+  standalone: true,
   imports: [
+    ReactiveFormsModule,
     CommonModule, 
-    FormsModule, 
     NzButtonModule, 
     NzInputModule, 
     NzFormModule, 
-    NzCardModule],
+    NzCardModule
+  ],
   templateUrl: './add-project.component.html',
   styleUrl: './add-project.component.css'
 })
 export class AddProjectComponent {
+  validateForm!: FormGroup;
+  isSubmitted = false;
+
   constructor(
-    private api:HttpClient,
-    private router:Router
+    private fb: FormBuilder,
+    private api: HttpClient,
+    private router: Router
   ) {}
 
-  apiUrl:string = 'http://localhost:3000/projects';
-
-  onCreate(data:any):void {
-    this.api.post(this.apiUrl, data).subscribe(res => {
-      if (res) {
-        alert("Thêm mới thành công!");
-        this.router.navigate([`projects`])
-      }
-    })
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      id: ['', [Validators.required]],
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      date: ['', Validators.required],
+      size: ['', [Validators.required, Validators.min(1), Validators.pattern("^[0-9]*$")]],
+    });
   }
 
-  onCancel():void {
+  onCreate(): void {
+    this.isSubmitted = true;
+
+    if (this.validateForm.invalid) {
+      this.validateForm.markAllAsTouched();
+      return;
+    }
+
+    this.api.post('http://localhost:3000/projects', this.validateForm.value).subscribe(res => {
+      if (res) {
+        alert("Thêm mới thành công!");
+        this.router.navigate(['projects']);
+      }
+    });
+  }
+
+  onCancel(): void {
     this.router.navigate(['projects']);
   }
 }

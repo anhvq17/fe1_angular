3a199150bc7994ec839a5445de0c34faf73a3d09
Vu Quoc Anh <hinm.aaaa@gmail.com>
2025-04-08 10:25:33 +0700

    update register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,6 @@ export const routes: Routes = [
   { path: 'dashboard', loadChildren: () => import('./team-leader/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES) },
   { path: 'projects', loadChildren: () => import('./team-leader/projects/projects.routes').then(m => m.PROJECTS_ROUTES) },
   { path: 'add-task', loadChildren: () => import('./employee/add-task/add-task.routes').then(m => m.ADDTASK_ROUTES) },
+  { path: 'register', loadChildren: () => import('./register/register.routes').then(m => m.REGISTER_ROUTES) },
+  { path: 'login', loadChildren: () => import('./register/register.routes').then(m => m.REGISTER_ROUTES) },
 ];
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,  
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  constructor(
+    private api: HttpClient,
+    private router: Router,
+  ) {}
 
+  apiUrl: string = `http://localhost:3000/users`;
+
+  onRegister(data:any):void {
+    this.api.post(this.apiUrl, data).subscribe(res => {
+      if (res) {
+        alert("Đăng ký thành công!");
+        this.router.navigate([`/login`])
+      }
+    })
+  }
 }

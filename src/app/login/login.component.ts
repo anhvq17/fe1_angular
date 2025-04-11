316fc793738695eb3fd80e932028b7e13760d048
Vu Quoc Anh <hinm.aaaa@gmail.com>
2025-04-11 22:47:13 +0700

    p2 register & login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, NzFormModule, NzInputModule, NzButtonModule, NzCardModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
   constructor(
     private auth: AuthService,
     private router: Router
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
 
 @Component({
   selector: 'app-register',
   standalone: true,  
-  imports: [FormsModule],
+  imports: [FormsModule, NzFormModule, NzInputModule, NzButtonModule, NzCardModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  email: string = '';
+  password: string = '';
+
   constructor(
     private auth: AuthService,
     private router: Router,
